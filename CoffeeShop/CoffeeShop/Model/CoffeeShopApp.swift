21dc253700478cb0e5bdf63c8cd1c10c6a8c5824
Bohdan <borysenkobohdan@gmail.com>
2025-04-09 13:56:39 +0300

    Start to project

---
+++ CoffeeShop/CoffeeShop/Model/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by Bohdan Borysenko on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
